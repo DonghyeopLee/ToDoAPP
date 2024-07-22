4f719819eb2ab5052a36104669db9f7b5b9790d6
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-07-23 00:32:55 +1000

    update

---
+++ api/routes/todo.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+
+todo_router = APIRouter(prefix="/api" , tags =["Todo"])
+
+@todo_router.get("/")
+def all_todos():
+    return "Not implemented"
+
+@todo_router.post("/")
+def post_todo():
+    return "Not implemented"
+
+
+@todo_router.put("/{key}")
+def update_todo(key: int):
+    return "Not implemented"
+
+
+@todo_router.delete("/{key}")
+def delete_todo(key: int):
+    return "Not implemented"
+
+
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from api.routes.todo import todo_router
+
 
 app = FastAPI()
+app.include_router(todo_router)
 
 @app.get("/")
 def index():
