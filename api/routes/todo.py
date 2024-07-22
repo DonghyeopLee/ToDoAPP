e3b1517ce30dbdc35a813ec1215fd2cc1d6204e5
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-07-23 01:07:41 +1000

    dv

--- api/routes/todo.py
+++ api/routes/todo.py
@@ -1,24 +1,38 @@
-from fastapi import APIRouter
+from fastapi import APIRouter , HTTPException, status
+from api.models.todo import Todo
+from api.schemas.todo import GetTodo, PostTodo,PutTodo
+
 
 todo_router = APIRouter(prefix="/api" , tags =["Todo"])
 
 @todo_router.get("/")
-def all_todos():
-    return "Not implemented"
+async def all_todos():
+    data = Todo.all()
+    return await GetTodo.from_queryset(data)
 
 @todo_router.post("/")
-def post_todo():
-    return "Not implemented"
+async def post_todo(body: PostTodo):
+    row = await Todo.create(**body.dict(exclude_unset=True))
+    return GetTodo.from_tortoise_orm(row)
 
 
 @todo_router.put("/{key}")
-def update_todo(key: int):
-    return "Not implemented"
+async def update_todo(key: int , body : PutTodo):
+    data = body.dict(exclude_unset= True)
+    exists = await Todo.filter(id=key).exists()
+    if not exists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Todo not found")
+    await Todo.filter(id=key).update(**data)
+    return await GetTodo.from_queryset_single(Todo.get(id=key))
 
 
 @todo_router.delete("/{key}")
-def delete_todo(key: int):
-    return "Not implemented"
+async def delete_todo(key: int):
+    exists = await Todo.filter(id=key).exists()
+    if not exists:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Todo not found")
+    await Todo.filter(id=key).delete()
+    return "Todo deleted successfully"
 
 
 
--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from api.routes.todo import todo_router
-
+from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
 app.include_router(todo_router)
+register_tortoise(
+    app=app,
+    db_url="sqlite://todo.db",
+    add_exception_handlers=True,
+    generate_schemas=True,
+    modules={"modules": ["api.models.todo"]}
+)
 
 @app.get("/")
 def index():
