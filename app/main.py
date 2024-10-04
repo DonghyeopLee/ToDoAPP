b328a7094099bf9487e274b834576120ce84c07d
Ian Lee <87459518+DonghyeopLee@users.noreply.github.com>
2024-10-04 22:45:49 +1000

    ffff

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from api.routes.todo import todo_router
 from tortoise.contrib.fastapi import register_tortoise
 
 app = FastAPI()
+
 app.include_router(todo_router)
 register_tortoise(
     app=app,
-    db_url="sqlite://todo.db",
+    db_url="sqlite://todos.db",
     add_exception_handlers=True,
     generate_schemas=True,
     modules={"modules": ["api.models.todo"]}
